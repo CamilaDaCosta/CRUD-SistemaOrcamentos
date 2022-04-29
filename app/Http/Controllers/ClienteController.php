1311db27809a1ae94853ad4115085b3507fb8578
Camila da Costa <camila.coosta99@gmail.com>
2022-04-29 16:48:02 -0300

    Visualizar Dados

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cliente;
+use App\Models\Endereco;
 
 class ClienteController extends Controller
 {
     public function clientes(){
-        return view('cliente.showall');
+        return view('cliente.create');
+    }
+
+    public function showall()
+    {
+        $cliente = Cliente::all();
+        return view('cliente/showall', ['cliente' => $cliente]);
     }
 }
