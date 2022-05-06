ddd4b84ece964acb2fad74d296f518b784705835
Raul José Pereira Neto <rauljose280@gmail.com>
2022-05-06 10:59:54 -0300

    Implementação de API CRUD Produto

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ClienteControllerApi;
+use App\Http\Controllers\ProdutoControllerApi;
+use App\Http\Resources\Produto;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// listar de todos os objetos
 Route::get('showall', [ClienteControllerApi::class, 'showall']);
+Route::get('produtos', [ProdutoControllerApi::class, 'shwoall']);
 
+// listar um objeto por ID
+Route::get('produto/{id}', [ProdutoControllerApi::class, 'show']);
+
+// salvar novo objeto
 Route::post('store', [ClienteControllerApi::class, 'store']);
+Route::post('produto', [ProdutoControllerApi::class, 'store']);
 
+// atualizar objeto
+Route::put('produto/{id}', [ProdutoControllerApi::class, 'update']);
+
+// deletar objeto
 Route::delete('delete/{id}', [ClienteControllerApi::class, 'destroy']);
+Route::delete('produto/{id}', [ProdutoControllerApi::class, 'destroy']);
